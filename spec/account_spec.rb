05a1b661208debaab28f398568d9443e10e2faac
Ahsan <rasul.ahsan@gmail.com>
2021-09-27 16:58:15 +0100

    basic deposit functionality added

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,4 +5,9 @@ describe Account do
     account = Account.new
     expect(account).to be_kind_of(Account)
   end
+
+  it 'can deposit money to the account' do
+    account = Account.new
+    expect { account.deposit(1000) }.to change { account.balance }.by(1000)
+  end
 end
