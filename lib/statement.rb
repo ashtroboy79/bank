79ea182a137edc4e9e8b4a71e815042fa8e796f7
Ahsan <rasul.ahsan@gmail.com>
2021-09-28 15:52:30 +0100

    can print out a statement with a single transaction

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,8 +1,16 @@
 class Statement
 
-  def print
-    puts '   date   || credit  || debit  || balance '
+  def print(transactions = [])
+    puts 'date       || credit  || debit  || balance '
+    transactions.reverse.each do | transaction | 
+      puts "#{transaction[:date]} || #{format(transaction[:credit])} || #{format(transaction[:debit])} || #{format(transaction[:balance])}"
+    end
+  end
 
+  def format(value)
+    if value.is_a?Integer 
+      value = "#{'%0.2f' % value}"
+    end
   end
 
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,7 +3,12 @@ require 'statement'
 describe Statement do
   it 'can print an empty statement' do
     statement = Statement.new
-    expect { statement.print }.to output("   date   || credit  || debit  || balance \n").to_stdout
+    expect { statement.print() }.to output("date       || credit  || debit  || balance \n").to_stdout
   end
 
+  it 'can print statement' do
+    statement = Statement.new
+    transaction = [{ :date => "28/09/2021", :credit => 1000, :debit => "", :balance => 1000 }]
+    expect { statement.print(transaction) }.to output("date       || credit  || debit  || balance \n28/09/2021 || 1000.00 ||  || 1000.00\n").to_stdout
+  end
 end
