fa0a85de4dc9dc70c3921a6639caa2e262aee13e
Ahsan <rasul.ahsan@gmail.com>
2021-09-28 15:57:08 +0100

    can print out a statement with a single transaction

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,15 +2,14 @@ class Statement
 
   def print(transactions = [])
     puts 'date       || credit  || debit  || balance '
-    transactions.each do | transaction | 
+    transactions.each do |transaction| 
       puts "#{transaction[:date]} || #{format(transaction[:credit])} || #{format(transaction[:debit])} || #{format(transaction[:balance])}"
     end
   end
 
   def format(value)
-    if value.is_a?Integer 
-      value = "#{'%0.2f' % value}"
-    end
+    return unless value.is_a? Integer 
+    value = "#{'%0.2f' % value}"
   end
 
 end
